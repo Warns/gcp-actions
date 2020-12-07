403c218579369302a7112973f984ec9506eeb1aa
tominaga <tominayu@square-enix.com>
2020-12-07 19:09:04 +0900

    Commit

--- vpc_network.tf
+++ vpc_network.tf
@@ -2,3 +2,8 @@ resource "google_compute_network" "dev-tominaga" {
   name                    = "dev-tominaga"
   auto_create_subnetworks = false
 }
+
+resource "google_compute_network" "hoge" {
+  name = "hoge"
+  auto_create_subnetworks = false
+}
